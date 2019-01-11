4b4f3df948fd918077a10569b9178903b5eded9e
Hayreddin Tuzel <hayreddin.tuzel@osf-commerce.com>
2019-01-11 15:15:44 +0300

    Challenge 3 is completed

---
+++ Challenge 3/script.js
@@ -0,0 +1,12 @@
+var inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+function handleUpdate() {
+    console.log(this);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
